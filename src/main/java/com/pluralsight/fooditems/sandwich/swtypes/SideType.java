8d85d5a0e6fca3f1824d8d24bbf4ef79ed9d5b16
lijas <lijason991@gmail.com>
2025-11-12 23:45:49 -0500

    fixed enum to no longer implement interface

--- src/main/java/com/pluralsight/fooditems/sandwich/swtypes/SideType.java
+++ src/main/java/com/pluralsight/fooditems/sandwich/swtypes/SideType.java
@@ -2,7 +2,7 @@ package com.pluralsight.fooditems.sandwich.swtypes;
 
 import com.pluralsight.fooditems.Calories;
 
-public enum SideType implements Calories {
+public enum SideType {
 
     FRIES,     // Fries have 375 calories
     PICKLES;    // Pickles have 10 calories
